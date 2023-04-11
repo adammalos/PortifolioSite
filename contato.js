727bbb940600252c003bd02a5a58500b76def3ca
Adam Macedo Malos <adamdomingosmm@gmail.com>
2023-04-10 23:47:50 -0300

    working on responsiveness

--- contato.js
+++ contato.js
@@ -1,2 +1,14 @@
+$(document).ready(function(){
+  var myCarousel = document.querySelector('#carouselExample');
+  var hammer = new Hammer(myCarousel);
+  
+  hammer.on('swipeleft', function(){
+    $('#carouselExample').carousel('next');
+  });
+  
+  hammer.on('swiperight', function(){
+    $('#carouselExample').carousel('prev');
+  });
+});
 
 
