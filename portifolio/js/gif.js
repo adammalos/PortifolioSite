a467c557494ebd3d50df8579c8635b817a760df4
Adam Macedo Malos <adamdomingosmm@gmail.com>
2024-03-19 01:07:52 -0300

    Produzindo modelo de disposição dos projetos

---
+++ portifolio/js/gif.js
@@ -0,0 +1,13 @@
+// Função para trocar a imagem quando o mouse estiver sobre ela
+function trocarImagem(id, novoSrc) {
+    var imagem = document.getElementById(id); // Obtém o elemento da imagem pelo ID
+    imagem.addEventListener("mouseover", function() { // Adiciona um ouvinte de evento para o mouseover
+        imagem.src = novoSrc; // Troca o src da imagem pelo novoSrc fornecido imediatamente
+    });
+    imagem.addEventListener("mouseout", function() { // Adiciona um ouvinte de evento para o mouseout
+        imagem.src = "img/site.PNG"; // Retorna ao src original quando o mouse sai da imagem imediatamente
+    });
+}
+
+// Chamar a função para trocar a imagem quando o mouse estiver sobre ela para gif1
+trocarImagem("gif1", "./gifs/unknown_2024.03.18-23.06_1.gif");
