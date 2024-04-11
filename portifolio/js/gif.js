c3520ee9e2859c3ada2f2774d2c1043ae7922b9e
Adam Macedo Malos <adamdomingosmm@gmail.com>
2024-04-11 20:49:52 -0300

    Ajustes 3

--- portifolio/js/gif.js
+++ portifolio/js/gif.js
@@ -1,12 +1,26 @@
-// Função para trocar a imagem quando o mouse estiver sobre ela
 function trocarImagem(id, novoSrc) {
     var imagem = document.getElementById(id); // Obtém o elemento da imagem pelo ID
-    imagem.addEventListener("mouseover", function() { // Adiciona um ouvinte de evento para o mouseover
-        imagem.src = novoSrc; // Troca o src da imagem pelo novoSrc fornecido imediatamente
-    });
-    imagem.addEventListener("mouseout", function() { // Adiciona um ouvinte de evento para o mouseout
-        imagem.src = "img/site.PNG"; // Retorna ao src original quando o mouse sai da imagem imediatamente
-    });
+    
+    // Função para trocar a imagem para o novoSrc
+    function trocarParaNovoSrc() {
+        imagem.src = novoSrc;
+    }
+    
+    // Função para trocar a imagem de volta para o src original
+    function trocarParaOriginalSrc() {
+        imagem.src = imagem.dataset.originalSrc; // Obtém o src original do dataset
+    }
+    
+    // Adiciona um ouvinte de evento para o mouseover
+    imagem.addEventListener("mouseover", trocarParaNovoSrc);
+    
+    // Adiciona um ouvinte de evento para o mouseout
+    imagem.addEventListener("mouseout", trocarParaOriginalSrc);
+    
+    // Salva o src original da imagem no dataset
+    imagem.dataset.originalSrc = imagem.src;
 }
+
 // Chamar a função para trocar a imagem quando o mouse estiver sobre ela para gif1
-trocarImagem("gif1", "./gifs/unknown_2024.03.18-23.06_1.gif");
+trocarImagem("gif1", "./gifs/curso.gif");
+trocarImagem("gif2", "./gifs/test.gif");
